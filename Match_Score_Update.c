a360fca7049101ebd62a962c9dea7e7a1d20f96d
mdabulkalamazad1425 <bsse1425@iit.du.ac.bd>
2023-09-29 23:49:46 +0600

    30.09.23

--- Match_Score_Update.c
+++ Match_Score_Update.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
@@ -15,7 +14,7 @@ struct Bowler
 {
     char playerName;
     int runsLossed;
-    int over;
+    int over[2];
     int wicket;
 };
 
@@ -25,6 +24,7 @@ struct Details
     struct Batsman batsman[11];
     struct Bowler bowler[11];
     int sixes;
+    int total_over[2];
     int fours;
     int Num_Wicket;
     int total_runs;
@@ -34,59 +34,121 @@ struct Details
 int number_of_overs=5;
 int number_of_players;
 int toss;
+int k=0,bow=0;
 
 
 void ScoreUpdate()
 {
     printf("Start Match\n\n ");
+    FILE *WS;
+     WS = fopen("ScoreFile.txt","w");
+    if(WS == NULL){
+        printf("Error occur ...\nCan't open ScoreFile.txt\n");
+        exit(0);
+    }
 
-    int k=0;
+
     for(int i=0;i<number_of_overs;i++)
     {
+            team[0].total_over[0] = i;
+
             int temp;
             for(int j=1;j<=6;j++)
             {
-                printf("\nOver %d.%d \nEnter  the score : ",i,j);
-                scanf("%d",&temp);
+                team[0].total_over[1] = j;
+                team[0].bowler[bow].over[1]=j;
+                //printf("\nOver %d.%d \nEnter  the score : ",i,j);
+                //scanf("%d",&temp);
+                temp = rand()%10;
+                fprintf(WS,"Ball %d.%d : %d\t",i,j,temp);
+
                 if(temp == 5)
                 {
                     j--;
-                    temp = 1;
+                    team[0].total_runs += 1;
+                    team[0].bowler[bow].runsLossed += temp;
+
+                    //temp = 1;
+                    //team[0].batsman[k].runsScored -= temp;
                 }
                 else if(temp == 7)
                 {
-                  j--;
-                  temp = rand()%6 + 1;
+                      j--;
+                      team[0].total_runs += 1;
+                      team[0].bowler[bow].runsLossed += 1;
 
 
+                      int extra = rand()%7 ;
+                      fprintf(WS,"extra : %d\t",extra);
+                      if (extra != 5){
+                             //temp=6;
+                            team[0].total_runs += extra;
+                            team[0].bowler[bow].runsLossed += extra;
+                            team[0].batsman[k].runsScored += extra;
+                            team[0].batsman[k].played_ball++;
+
+                      }
+                      team[0].batsman[k].played_ball++;
 
                 }
                 else if(temp == 8)
                 {
                     temp = rand()%4 + 1;
+                    fprintf(WS,"By : %d\t",temp);
+                    team[0].total_runs += temp;
+                    team[0].bowler[bow].runsLossed += temp;
+                    team[0].batsman[k].runsScored += temp;
+                    team[0].batsman[k].played_ball++;
                 }
                 else if(temp == 9)
                 {
-                  team[0].Num_Wicket++;
-                  team[0].bowler[0].wicket++;
+                      printf("Player %d has been out :");
+                      int out = rand()%3 +1;
+                      fprintf(WS,"out %d\t",out);
+
+                      if(out == 1){
+                        printf("\t\tBold Out\n");
+                        team[0].bowler[bow].wicket++;
+                        k=k+2;
+                      }
+                      else if(out == 2){
+                        printf("\n\t\tCatch Out\n");
+                        team[0].bowler[bow].wicket++;
+                        k+= rand()%2+1;
+                      }
+                      else{
+                            printf("\n\t\tRun Out\n");
+                            k+= rand()%2+1;
+                      }
+
+                      team[0].Num_Wicket++;
+
+
                 }
+                else{
 
 
-                if(temp == 6 || temp ==7)
-                {
-                    team[0].sixes++;
+                        if(temp == 6)
+                        {
+                            team[0].sixes++;
+                        }
+                        else if(temp == 4 )
+                        {
+                            team[0].fours++;
+                        }
+
+
+                        team[0].total_runs += temp;
+                        team[0].batsman[k].runsScored += temp;
+                        team[0].bowler[bow].runsLossed += temp;
+                        team[0].batsman[k].played_ball++;
+
                 }
-                else if(temp == 4 || temp == 5)
-                {
-                 team[0].fours++;
-                }
+                DisplayScoreBoard();
 
 
-                team[0].total_runs += temp;
-                team[0].batsman[k].runsScored += temp;
-                printf("\nplayer %d : run %d\n",k,team[0].batsman[k].runsScored);
-                printf("Total %d\n",team[0].total_runs);
-                if((temp<4 && temp%2 != 0) || j==6){
+
+                if( temp==1 || temp ==3 ){
                     if(k == team[0].Num_Wicket){
                         k=k+1;
                     }
@@ -95,22 +157,39 @@ void ScoreUpdate()
                     }
 
                 }
-
-
-
-
+                fprintf(WS,"\n");
+                 printf("\nNext..");
+                 getchar();
 
             }
 
+            if(k == team[0].Num_Wicket)
+            {
+                k=k+1;
+            }
+            else
+            {
+                k=team[0].Num_Wicket;
+            }
+            team[0].bowler[bow].over[0]++;
+            bow++;
+            if(bow==5){
+                bow=0;
+            }
+
+
+
+
     }
-
-
-
-
-
+    fclose(WS);
 }
-int main(){
+void DisplayScoreBoard(){
+    printf("Team - 1: \n");
+    printf("Total : %d\tWicket : %d\tPlayer[%d]\tRun Lossed : %d\n",team[0].total_runs,team[0].Num_Wicket,k,team[0].batsman[k].runsScored);
+    printf("over %d.%d\n",team[0].bowler[bow].over[0],team[0].bowler[bow].over[1]);
+    return;
+}
+/*int main(){
     ScoreUpdate();
     return 0;
-
-}
+}*/
