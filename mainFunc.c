b65d4aa462346bb0be7454101063b21db8d8c466
abulkalamazad1425 <141701794+abulkalamazad1425@users.noreply.github.com>
2023-12-14 12:27:55 +0600

    After adding 6th and 7th option

--- mainFunc.c
+++ mainFunc.c
@@ -1,33 +1,42 @@
-//#include<stdio.h>
+#include<stdio.h>
 
-//int main(){
-//
-//    printf("\n\n\t\t\tCricket Score Board\n\n");
-//    int option=1;
-//    while(option){
-//            printf("1. Upcoming Matches.\n");
-//            printf("2. Schedule a Tournament.\n");
-//
-//            printf("3. Player Information.\n");
-//            printf("4. Ongoing Match Score Update.\n");
-//            printf("5. Predict the winner.\n");
-//            printf("Select One( 0 for quit) : ");
-//            scanf("%d",&option);
-//
-//            if(option==1)
-//                UpcomingMatch();
-//            else if(option == 2)
-//                PremierLeagueScheduling();
-//            else if(option == 3)
-//                PlayerInformation();
-//            else if(option == 4){
-//                UpdateScore();
-//            }
-//            else if(option == 5){
-//                Prediction();
-//            }
-//            else
-//                printf("Choose correctly");
-//    }
-//    return 0;
-//}
+int main(){
+
+     printf("\n\n\t\t\tCricket Score Board\n\n");
+    int option=1;
+    while(option){
+            printf("1. Upcoming Matches.\n");
+            printf("2. Schedule a Tournament.\n");
+
+            printf("3. Player Information.\n");
+            printf("4. Ongoing Match Score Update.\n");
+            printf("5. Predict the winner by Decission Tree.\n");
+            printf("6. Show Player Rank List.\n");
+            printf("7. Predict the winner by Linear Regression.\n");
+            printf("Select One( 0 for quit) : ");
+            scanf("%d",&option);
+            printf("\n\n\n");
+
+            if(option==1)
+                UpcomingMatch();
+            else if(option == 2)
+                PremierLeagueScheduling();
+            else if(option == 3)
+                PlayerInformation();
+            else if(option == 4){
+                UpdateScore();
+            }
+            else if(option == 5){
+                Prediction();
+            }
+            else if(option == 6){
+                ListOfPlayerRanking();
+            }
+            else if(option == 7){
+                predictByRegression();
+            }
+            else
+                printf("Choose correctly");
+    }
+    return 0;
+}
